602f361cdda7a000be4b61e3d998c3dc9a2e6c24
kaprisa57@gmail.com <kaprisa57@gmail.com>
2018-07-24 04:26:04 +0800

    Оптимизированы ключи для Redis

--- controllers/books/get.js
+++ controllers/books/get.js
@@ -5,11 +5,12 @@ export default router => {
     router.get('/', async ctx => {
         const query = ctx.request.query
         const condition = query.like ? `title LIKE '${query.like}%'` : 1
-        const queryString = `SELECT b.*, u.name FROM books b JOIN users u ON b.author_id=u.id where ${condition} ORDER BY ${query.order || 'created_at'} ${query.direction || 'ASC'} ${query ? `LIMIT ${query.offset}, ${query.limit}` : 1};`
-        let books = await getCached(queryString)
+        const key = `${condition}${query.order || 'created_at'}${query.direction || 'ASC'}${query.offset}${query.limit}`
+        const queryString = `SELECT b.*, u.name FROM books b JOIN users u ON b.author_id=u.id where ${condition} ORDER BY ${query.order || 'created_at'} ${query.direction || 'ASC'} ${`LIMIT ${query.offset}, ${query.limit}`};`
+        let books = await getCached(key)
         if (!books) {
             books = await Book.get(queryString)
-            await setCached(queryString, JSON.stringify(books))
+            await setCached(key, JSON.stringify(books))
         }
         const count = await Book.count({ condition })
         ctx.body = { books, count }
